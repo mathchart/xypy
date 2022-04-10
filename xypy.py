e9029b80f748b21a3ea628cc04bcbc9c8741b2b7
Mathchart <6526594+mathchart@users.noreply.github.com>
2022-04-10 15:19:25 -0400

    Add files via upload

--- xypy.py
+++ xypy.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import sys
+from random import randint
 
+from myfuncs import randcol
 pg.init()
 
 """Color constants"""
@@ -10,6 +12,7 @@ BLACK = (0,0,0)
 YELLOW = (255,255,0)
 BLUE = (0,0,255)
 WHITE = (255,255,255)
+GREY = (220,220,220)
 
 
 """X and Y coordinates for drawings. Consider lists of objects for animation rewards"""
@@ -55,13 +58,24 @@ big_screen = pg.display.set_mode((w,h))
 pg.display.set_caption("Connect the dots!")
 ccs = pg.Surface((w,h))
 ccs.fill(WHITE)
+pg.draw.rect(ccs,GREY,(1.5*cell_w,0,10.5*cell_w,13*cell_w-cell_w//2))
+pg.draw.rect(ccs,WHITE,(1.5*cell_w,2.5*cell_h,10*cell_w,10*cell_h))
 pg.draw.rect(ccs,BLUE,(0,0,1.5*cell_w,13*cell_w-cell_w//2))
 pg.draw.rect(ccs,RED,(0,13*cell_w-cell_w//2,1.5*cell_w,5.5*cell_w))
 pg.draw.rect(ccs,YELLOW,(1.5*cell_w,13*cell_w-cell_w//2,11*cell_w,6*cell_h))
+
+"""Draw diagonal of squares"""
+start_x = int(cell_w * 1.5 )
+start_y = int(cell_h*11.5)
+for i in range(11):
+    x = start_x + i*cell_w
+    y = start_y - i*cell_h
+    pg.draw.rect(ccs,GREY,(x,y,cell_w,cell_h))
+
 """Draw X and Y axes """
 for x in range(cell_w//2,w,cell_w):
     pg.draw.line(ccs,(0,0,0),(x,0),(x,h),2)
-pg.draw.line(ccs,BLACK,(1.5*cell_w,0),(1.5*cell_w,h),18)
+pg.draw.line(ccs,BLACK,(1.5*cell_w,0),(1.5*cell_w,h),22)
 for y in range(cell_w//2,h,cell_w):
     pg.draw.line(ccs,(0,0,0),(0,y),(w,y),2)
 pg.draw.line(ccs,BLACK,(0,13*cell_w-cell_w//2),(w,13*cell_w-cell_w//2),22)
@@ -72,12 +86,13 @@ for x in range(cell_w//2,w,cell_w):
         pg.draw.circle(ccs,BLACK,(x,y),5)
 
 """Draw index numbers on the X and Y axes"""
-font = pg.font.SysFont('cambria', 18)
-for n in range(1,18):
+font = pg.font.SysFont('Helvetica', 18)
+for n in range(0,18):
     num  = font.render(str(12-n),1,WHITE)
     ccs.blit(num,(cell_h*1.4,cell_h*n+16))
 for n in range(-1,13):
-    if n == 0: continue
+    if n == 0: 
+        continue
     num  = font.render(str(n),1,WHITE)
     ccs.blit(num,(cell_w*n+1.4*cell_w,13*cell_w-cell_w//2-10))
 
@@ -88,30 +103,40 @@ ccs.blit(xy,(6*cell_w,14*cell_h))
 """Draw dot multipication sign between X and Y"""
 pg.draw.circle(ccs,WHITE,(8.5*cell_w,15.5*cell_h),10)
     
-"""Draw products in corners for later"""
-# font = pg.font.SysFont('miriam', 20)
-# px = 0
-# for x in range(cell_w//2,w,cell_w):
-#     px += 1
-#     py = 0
-#     for y in range(cell_w//2,h,cell_w):
-#         py += 1
-#         if py > 1 and py < 11 and px > 1 and px < 11:
-#             product = font.render(str((px)*(py)),1,BLACK)
-#             ccs.blit(product,(x,y))
+"""Draw products in corners for pattern scripts"""
+font = pg.font.SysFont('Helvetica', 22)
+px = 0
+for x in range(cell_w//2+cell_w*2-22,w,cell_w):
+    px += 1
+    py = 11
+    for y in range(cell_h//2+2*cell_h,h,cell_h):
+        py -= 1
+        if py > 0 and py < 11 and px > 0 and px < 11:
+            product = font.render(str((px)*(py)),1,BLACK)
+            ccs.blit(product,(x,y))
        
 #***********************************************
 
 clock = pg.time.Clock()
 FPS = 60
+fig = randint(0,len(xys)-1)
+points = zip(xys[fig][0],xys[fig][1])
+locs = []
+for point in points:
+    locs.append((point[0]*cell_w+95,h - point[1]*cell_h-420))
+for point in locs:
+    print(point)
 
 big_screen.fill((255,255,255))
 while True:
     clock.tick(FPS)
+    
     for ev in pg.event.get():
         if ev.type == pg.QUIT or (ev.type == pg.KEYDOWN and pg.K_ESCAPE): 
             pg.display.quit()       
             sys.exit()
     
     big_screen.blit(ccs,(0,0))
+    #pg.draw.polygon(big_screen,(150,150,0),locs,14)
+    pg.draw.lines(big_screen,1,(150,150,0),locs,14)
     pg.display.flip()
